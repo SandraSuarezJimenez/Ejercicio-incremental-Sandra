b3d47cb8a4da067d5c22078ef6920f29873ba7ca
SandraSuarezJimenez <1983ssj@gmail.com>
2023-03-20 09:38:48 +0100

    Ejercicio 11 completo

---
+++ ej_incr_11.js
@@ -0,0 +1,15 @@
+var nombre;
+var apellidos;
+var fechaDeNacimiento;
+var cursosDeProgramacionRealizados;
+
+nombre = prompt("Introduce tu nombre");
+apellidos = prompt("Introduce tus apellidos");
+fechaDeNacimiento = prompt("Introduce tu año de nacimiento")
+cursosDeProgramacionRealizados = prompt("Introduce el número de cursos de programación realizados")
+
+cursosDeProgramacionRealizados = cursosDeProgramacionRealizados + 1
+
+if(fechaDeNacimiento <2000){
+    alert("¿Estás seguro de que eres un programador Junior?" )
+}
